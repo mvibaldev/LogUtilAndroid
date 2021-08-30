28d90080f8dc6c56768a730b0cda9264adcf1e84
mvibaldev <mvibaldev1@gmail.com>
2021-08-30 15:30:43 -0500

    create android project library

---
+++ LogUtil/src/main/java/mx/ine/logutil/LogDebug.java
@@ -0,0 +1,13 @@
+package mx.ine.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static  final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+
+    }
+}
